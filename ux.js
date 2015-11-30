593eedbf65ef21f27e0574ce28eba25bf114a636
Minsup Ko <atsundwn@gmail.com>
2015-11-30 17:16:22 -0500

    Adds the single page div into index.html. -mk
    Adds product id as data attribute in product div for later clickhandler use.

--- ux.js
+++ ux.js
@@ -4,19 +4,26 @@ var ux = {
 
   login: function(){
     $('#main-product-page').hide();
+    $('#single-product-page').hide();
   },
 
   skipLogin: function(){
     $('#login-overlay').hide();
     $('#main-product-page').show(500);
+    $('#single-product-page').hide();
   },
 
   afterLogin: function(){
     $('#login-overlay').hide();
     $('#main-product-page').show(500);
+    $('#single-product-page').hide();
   },
 
-  mainProductPage: function(){}
+  singleProductPage: function(){
+    $('#login-overlay').hide();
+    $('#main-product-page').hide();
+    $('#single-product-page').show(500);
+  }
 
 
 };
